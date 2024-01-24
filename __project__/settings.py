6cc268ead045eb78c53ba9879becceeba2a7a644
Raphael <raphael.mukha@gmail.com>
2024-01-24 14:17:18 +0300

    Setup DATABASES in settings.py

--- __project__/settings.py
+++ __project__/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -53,10 +55,13 @@ TEMPLATES = [
 WSGI_APPLICATION = '__project__.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv(
+            'DATABASE_URL',
+            'postgresql://pguser:pgpass@localhost:5432/pgdb'
+        ),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
