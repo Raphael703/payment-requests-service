3556ae59068b91f469c83818b843e327f200e045
Raphael <raphael.mukha@gmail.com>
2024-01-24 14:22:20 +0300

    Add choices: PAYMENT_REQUEST_STATUS, PAYMENT_TYPES, CARD_TYPES and CHECKING_ACCOUNT_TYPES

---
+++ payment_requests/consts.py
@@ -0,0 +1,33 @@
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
+
+PAYMENT_REQUEST_STATUS = Choices(
+    (0, 'AWAITING', _('Ожидает оплаты')),
+    (1, 'PAID', _('Оплачена')),
+    (2, 'CANCELLED', _('Отменена'))
+)
+
+PAYMENT_TYPES = Choices(
+    (0, 'CARD', _('Карта')),
+    (1, 'CHECKING_ACCOUNT', _('Платежный счет'))
+)
+
+CARD_TYPES = Choices(
+    (0, 'DEBIT', _('DEBIT')),
+    (1, 'CREDIT', _('CREDIT')),
+    (2, 'PREPAID', _('PREPAID')),
+    (3, 'SAVINGS', _('SAVINGS')),
+    (4, 'PLATINUM', _('PLATINUM')),
+    (5, 'TRAVEL', _('TRAVEL')),
+    (6, 'LOYALTY', _('LOYALTY'))
+)
+
+CHECKING_ACCOUNT_TYPES = Choices(
+    (0, 'CURRENT', _('Текущий (чековый)')),
+    (1, 'SAVINGS', _('Сберегательный')),
+    (4, 'BUSINESS', _('Бизнес')),
+    (5, 'PAYROLL', _('Зарплатный')),
+    (6, 'STUDENT', _('Студенческий')),
+    (7, 'JOINT', _('Совместный')),
+    (8, 'HIGH_INTEREST', _('С высокой процентной ставкой'))
+)
