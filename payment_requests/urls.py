7271d776afe25da87db5b0a72623b3dac5207178
Raphael <raphael.mukha@gmail.com>
2024-01-24 21:16:49 +0300

    Add CheckingAccountListView to urlpatterns in app

--- payment_requests/urls.py
+++ payment_requests/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from payment_requests.views import PaymentRequestListView
+from payment_requests.views import PaymentRequestListView, CheckingAccountListView
 
 urlpatterns = [
-    path('', PaymentRequestListView.as_view(), name='payment_requests_list')
+    path('', PaymentRequestListView.as_view(), name='payment_requests_list'),
+    path('checking_account/', CheckingAccountListView.as_view(), name='checking_account_list'),
 ]
