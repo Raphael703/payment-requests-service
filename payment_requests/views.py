c53e22e5151b8bac935ae89924e4cb0853c34934
Raphael <raphael.mukha@gmail.com>
2024-01-24 21:16:36 +0300

    Implement CheckingAccountListView

--- payment_requests/views.py
+++ payment_requests/views.py
@@ -1,11 +1,19 @@
 from django.views.generic import ListView
 
-from payment_requests.models import PaymentRequest
+from payment_requests.models import PaymentRequest, CheckingAccount
 
 
 class PaymentRequestListView(ListView):
     model = PaymentRequest
-    template_name = 'payment_requests/list.html'
+    template_name = 'payment_requests/payment_request/list.html'
+    context_object_name = 'payment_requests'
+
+    paginate_by = 16
+
+
+class CheckingAccountListView(ListView):
+    model = CheckingAccount
+    template_name = 'payment_requests/checking_account/list.html'
     context_object_name = 'payment_requests'
 
     paginate_by = 16
