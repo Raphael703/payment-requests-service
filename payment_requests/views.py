aa9ee7d102f0c4c7536c80777bd5caf6436b2d31
Raphael <raphael.mukha@gmail.com>
2024-01-24 18:42:21 +0300

    Implement PaymentRequestListView

--- payment_requests/views.py
+++ payment_requests/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from payment_requests.models import PaymentRequest
+
+
+class PaymentRequestListView(ListView):
+    model = PaymentRequest
+    template_name = 'payment_requests/list.html'
+    context_object_name = 'payment_requests'
+
+    paginate_by = 16
