1b9eb395bd590eeabc9d228026a821debe2eaaa5
Raphael <raphael.mukha@gmail.com>
2024-01-24 22:40:04 +0300

    Expand CheckingAccountFilerListView by CheckingAccountFilterSet

--- payment_requests/views.py
+++ payment_requests/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
+from django_filters.views import FilterView
 
+from payment_requests.filters import CheckingAccountFilterSet
 from payment_requests.models import PaymentRequest, CheckingAccount
 from payment_requests.sorters import SortableListMixin, SortField
 
@@ -12,9 +14,11 @@ class PaymentRequestListView(ListView):
     paginate_by = 16
 
 
-class CheckingAccountListView(SortableListMixin, ListView):
+class CheckingAccountFilerListView(SortableListMixin, FilterView):
     model = CheckingAccount
     template_name = 'payment_requests/checking_account/list.html'
     context_object_name = 'payment_requests'
+
     paginate_by = 16
     default_sort_field = SortField('id')
+    filterset_class = CheckingAccountFilterSet
