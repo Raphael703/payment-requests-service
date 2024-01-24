877d3b14a93c9e8b86ae8210768061f98411b451
Raphael <raphael.mukha@gmail.com>
2024-01-24 22:40:20 +0300

    Update url path for CheckingAccountFilerListView

--- payment_requests/urls.py
+++ payment_requests/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from payment_requests.views import PaymentRequestListView, CheckingAccountListView
+from payment_requests.views import PaymentRequestListView, CheckingAccountFilerListView
 
 urlpatterns = [
     path('', PaymentRequestListView.as_view(), name='payment_requests_list'),
-    path('checking_account/', CheckingAccountListView.as_view(), name='checking_account_list'),
+    path('checking_account/', CheckingAccountFilerListView.as_view(), name='checking_account_list'),
 ]
