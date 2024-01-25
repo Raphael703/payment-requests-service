91771fd9665b462b458af8ea346bea3ee8937fc7
Raphael <raphael.mukha@gmail.com>
2024-01-25 16:14:07 +0300

    Expand User(Create/Logout/Login)Views by SuccessMessageMixin

--- users/views.py
+++ users/views.py
@@ -1,25 +1,34 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
+from django.utils.translation import gettext_lazy as _
 
 from users.forms import CustomUserCreationForm
+from users.mixins import AdminRequiredMixin
 
 
-class UserCreateView(CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
     template_name = 'users/create.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
+    success_message = _('Аккаунт успешно создан')
 
 
-class UserLoginView(LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'users/login.html'
     next_page = reverse_lazy('index')
+    success_message = _('Вы успешно авторизовались')
 
 
-class UserLogoutView(LogoutView):
+class UserLogoutView(SuccessMessageMixin, LogoutView):
     next_page = reverse_lazy('index')
 
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, _('Вы вышли из системы'))
+        return super().dispatch(request, *args, **kwargs)
 
 class UsersListView(ListView):
     model = get_user_model()
