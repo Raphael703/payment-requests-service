660346204ea8ffb553fdc1e7b657e59feb3f3a12
Raphael <raphael.mukha@gmail.com>
2024-01-25 11:58:44 +0300

    Add UserCreateView, UserLoginView, UserLogoutView to main urlpatterns

--- __project__/urls.py
+++ __project__/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from users.views import UserLoginView, UserLogoutView, UserCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('sign-up/', UserCreateView.as_view(), name='sign_up'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
     path('payment_requests/', include('payment_requests.urls'))
 ]
