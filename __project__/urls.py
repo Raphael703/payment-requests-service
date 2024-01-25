fbd77689fa3ceb17ea3c8fadd3578d59c07d1a81
Raphael <raphael.mukha@gmail.com>
2024-01-25 20:18:34 +0300

    Rebase main urlpatterns

--- __project__/urls.py
+++ __project__/urls.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from users.views import UserLoginView, UserLogoutView, UserCreateView, UsersListView
+from __project__.yasg import urlpatterns as yasg_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('sign-up/', UserCreateView.as_view(), name='sign_up'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('users/', UsersListView.as_view(), name='users_list'),
-    path('payment_requests/', include('payment_requests.urls'))
+    path('users/', include('users.urls')),
+    path('payment-requests/', include('payment_requests.urls'))
 ]
+urlpatterns += yasg_urlpatterns
