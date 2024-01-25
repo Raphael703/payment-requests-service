5eab2647b1155a8382e1f67938d0ee7c42fdac54
Raphael <raphael.mukha@gmail.com>
2024-01-25 16:14:59 +0300

    Expand PaymentRequestListView, CheckingAccountFilerListView by AdminRequiredMixin and CustomLoginRequiredMixin

--- payment_requests/views.py
+++ payment_requests/views.py
@@ -4,9 +4,10 @@ from django_filters.views import FilterView
 from payment_requests.filters import CheckingAccountFilterSet
 from payment_requests.models import PaymentRequest, CheckingAccount
 from payment_requests.sorters import SortableListMixin, SortField
+from users.mixins import AdminRequiredMixin, CustomLoginRequiredMixin
 
 
-class PaymentRequestListView(ListView):
+class PaymentRequestListView(AdminRequiredMixin, ListView):
     model = PaymentRequest
     template_name = 'payment_requests/payment_request/list.html'
     context_object_name = 'payment_requests'
@@ -14,7 +15,7 @@ class PaymentRequestListView(ListView):
     paginate_by = 16
 
 
-class CheckingAccountFilerListView(SortableListMixin, FilterView):
+class CheckingAccountFilerListView(CustomLoginRequiredMixin, SortableListMixin, FilterView):
     model = CheckingAccount
     template_name = 'payment_requests/checking_account/list.html'
     context_object_name = 'payment_requests'
