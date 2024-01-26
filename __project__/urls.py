d0f918d13e0f2f438c4748368a35320e620ec4a9
Raphael <raphael.mukha@gmail.com>
2024-01-26 18:44:21 +0300

    Update main urls.py for static

--- __project__/urls.py
+++ __project__/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -11,3 +13,6 @@ urlpatterns = [
     path('payment-requests/', include('payment_requests.urls'))
 ]
 urlpatterns += yasg_urlpatterns
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
